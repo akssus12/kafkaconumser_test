3298672126d000cc0bbc7a68c3987ae22e2c61ca
yeonwoo-jeong <0496sky@naver.com>
2023-07-13 10:17:56 +0000

    consumer commit

---
+++ src/main/java/com/dcclab/examples/streams/consumer/Consumer.java
@@ -0,0 +1,41 @@
+package com.dcclab.examples.streams.consumer;
+
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Consumer {
+
+    public static void main(String[] args) throws Exception{
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	props.put("group.id", "karim-group-id-1"); // group-id 설정
+	props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // key deserializer
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); // value deserializer
+
+	KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+	consumer.subscribe(Collections.singletonList("wordcount-output"));
+	
+	String message = null;
+        try {
+            while(true) {
+                ConsumerRecords<String, String> records = consumer.poll(100);
+
+                for (ConsumerRecord<String, String> record : records) {
+                    message = record.value();
+                    System.out.println(message);
+                }
+            }
+        } catch(Exception e) {
+        } finally {
+            consumer.close();
+        }
+    }
+}
